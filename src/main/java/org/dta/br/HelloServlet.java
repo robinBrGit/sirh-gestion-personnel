0954ecfe3351938fd0c51ecf7b8a1f97a7f5a2d5
Robin BROQUERIE <robin.broquerie@gmail.com>
2019-07-01 12:09:38 +0200

    second commit

--- src/main/java/org/dta/br/HelloServlet.java
+++ src/main/java/org/dta/br/HelloServlet.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
 
-
+    //GET /demo
     public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
             throws IOException {
         httpServletResponse.getWriter().print("Hello from servlet");
